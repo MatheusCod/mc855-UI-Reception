1ef4c6c147fcf83911d2ce60f06c7b3e6aa1f48d
MatheusCod <matheus.log100@gmail.com>
2022-06-14 02:04:15 -0300

    feat: add Login component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'mc855-UI-Reception'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mc855-UI-Reception');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mc855-UI-Reception app is running!');
-  });
 });
