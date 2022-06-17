22f5c6b4480a171099b01b7da0a91d687494ca3b
MatheusCod <matheus.log100@gmail.com>
2022-06-17 18:22:41 -0300

    feat: add patient card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { PatientCardComponent } from './patient-card/patient-card.component';
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PatientCardComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +31,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    MatDividerModule,
+    MatRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
